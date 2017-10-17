dddc2bbdada8e0800d95f09f58e50d5aa47fbf93
simmu <simranbhangu01@gmail.com>
2017-10-17 14:35:17 +0530

    version 0.1.1

--- plutodrone/src/PlutoNode.cpp
+++ plutodrone/src/PlutoNode.cpp
@@ -9,7 +9,9 @@
 #include <unistd.h>
 #include <plutodrone/Common.h>
 #include <plutodrone/Protocol.h>
-// #include <plutodrone/PlutoMsg.h>
+#include <plutodrone/PlutoMsg.h>
+// #include <plutodrone/JoystickClient.h>
+// #include <plutodrone/Position.h>
 
 
 #define PORT 23
@@ -65,7 +67,6 @@ void *serviceFunction(void *threadid)
   {
       if (serviceClient.call(service))
       {
-       //  ROS_INFO("Sum: %ld", (long int)service.response.rcAUX1);
        userRC[0]=service.response.rcRoll;
        userRC[1]=service.response.rcPitch;
        userRC[2]=service.response.rcThrottle;
@@ -87,11 +88,20 @@ void *serviceFunction(void *threadid)
        service.request.pitch=pitch;
        service.request.yaw=yaw;
        service.request.alt=alt;
-       
-    //usleep(1000);
-  }
+      }
   }
  pthread_exit(NULL);
+}
+void Callback(const plutodrone::PlutoMsg::ConstPtr& msg)
+{
+ userRC[0] = msg->rcRoll;
+ userRC[1] = msg->rcPitch;
+ userRC[2] = msg->rcThrottle;
+ userRC[3] = msg->rcYaw;
+ userRC[4] = msg->rcAUX1;
+ userRC[5] = msg->rcAUX2;
+ userRC[6] = msg->rcAUX3;
+ userRC[7] = msg->rcAUX4;
 }
 int main(int argc, char **argv)
 {
@@ -99,7 +109,8 @@ int main(int argc, char **argv)
     int rc;
     ros::init(argc, argv, "plutonode");
     ros::NodeHandle n;
-    rc = pthread_create(&thread, NULL, createSocket, 	(void *)1);
+    ros::Subscriber sub = n.subscribe("drone_command", 1000, Callback);
+    rc = pthread_create(&thread, NULL, createSocket,  (void *)1);
     if (rc)
     {
      cout << "Error:unable to create communication thread," << rc << endl;
@@ -110,14 +121,14 @@ int main(int argc, char **argv)
     if(isSocketCreate)
     {
       //cout << "main() : creating write thread, " << i << endl;
-      rc = pthread_create(&writeThread, NULL, writeFunction, 	(void *)2);
+      rc = pthread_create(&writeThread, NULL, writeFunction,  (void *)2);
       if (rc)
       {
         cout << "Error:unable to create write thread," << rc << endl;
         exit(-1);
       }
       // cout << "main() : creating read thread, " << i << endl;
-      rc = pthread_create(&readThread, NULL, readFunction, 	(void *)3);
+      rc = pthread_create(&readThread, NULL, readFunction,  (void *)3);
       if (rc)
       {
         cout << "Error:unable to read create thread," << rc << endl;
@@ -126,7 +137,7 @@ int main(int argc, char **argv)
 
       serviceClient = n.serviceClient<plutodrone::PlutoPilot>("PlutoService",true);
       //cout << "main() : creating service thread, " << i << endl;
-      rc = pthread_create(&serviceThread, NULL, serviceFunction, 	(void *)4);
+      rc = pthread_create(&serviceThread, NULL, serviceFunction,  (void *)4);
       
       if (rc)
       {
