65e1e4997812692a5c294aeeb43da1465d3ee347
badrobot <badrobot161@gmail.com>
2018-06-11 11:56:02 +0530

    Added Multi Drone Support

--- plutodrone/src/PlutoNode.cpp
+++ plutodrone/src/PlutoNode.cpp
@@ -29,13 +29,12 @@ plutodrone::PlutoPilot service;
 
 int userRC[8]={0,0,0,0,0,0,0,0};
 
-void *createSocket(void *threadid)
-{
+void *createSocket(void *threadid){
  isSocketCreate=com.connectSock();
  pthread_exit(NULL);
 };
-void *writeFunction(void *threadid)
-{
+
+void *writeFunction(void *threadid){
   std::vector<int> requests;
   requests.push_back(MSP_RC);
   requests.push_back(MSP_ATTITUDE);
@@ -51,30 +50,30 @@ void *writeFunction(void *threadid)
   pthread_exit(NULL);
 }
 
-void *readFunction(void *threadid)
-{
+void *readFunction(void *threadid){
   do 
   {
-      com.readFrame();
+    com.readFrame();
     //usleep(5);
   }
   while(1);
   pthread_exit(NULL);
 }
-void *serviceFunction(void *threadid)
-{
+
+void *serviceFunction(void *threadid){
   while (1) 
   {
       if (serviceClient.call(service))
       {
-       userRC[0]=service.response.rcRoll;
-       userRC[1]=service.response.rcPitch;
-       userRC[2]=service.response.rcThrottle;
-       userRC[3]=service.response.rcYaw;
-       userRC[4]=service.response.rcAUX1;
-       userRC[5]=service.response.rcAUX2;
-       userRC[6]=service.response.rcAUX3;
-       userRC[7]=service.response.rcAUX4;
+       //  ROS_INFO("Sum: %ld", (long int)service.response.rcAUX1);
+       // userRC[0]=service.response.rcRoll;
+       // userRC[1]=service.response.rcPitch;
+       // userRC[2]=service.response.rcThrottle;
+       // userRC[3]=service.response.rcYaw;
+       // userRC[4]=service.response.rcAUX1;
+       // userRC[5]=service.response.rcAUX2;
+       // userRC[6]=service.response.rcAUX3;
+       // userRC[7]=service.response.rcAUX4;
        service.request.accX=accX;
        service.request.accY=accY;
        service.request.accZ=accZ;
@@ -88,12 +87,14 @@ void *serviceFunction(void *threadid)
        service.request.pitch=pitch;
        service.request.yaw=yaw;
        service.request.alt=alt;
-      }
+       // cout << alt << endl;
+    //usleep(1000);
+  }
   }
  pthread_exit(NULL);
 }
-void Callback(const plutodrone::PlutoMsg::ConstPtr& msg)
-{
+
+void Callback(const plutodrone::PlutoMsg::ConstPtr& msg){
  userRC[0] = msg->rcRoll;
  userRC[1] = msg->rcPitch;
  userRC[2] = msg->rcThrottle;
@@ -102,15 +103,16 @@ void Callback(const plutodrone::PlutoMsg::ConstPtr& msg)
  userRC[5] = msg->rcAUX2;
  userRC[6] = msg->rcAUX3;
  userRC[7] = msg->rcAUX4;
+ // cout << "roll = " << userRC[0] << "pitch = " <<userRC[1] << "Throttle = "<< userRC[2]  << endl;
 }
-int main(int argc, char **argv)
-{
+
+int main(int argc, char **argv){
     pthread_t thread, readThread, writeThread, serviceThread;
     int rc;
     ros::init(argc, argv, "plutonode");
     ros::NodeHandle n;
     ros::Subscriber sub = n.subscribe("drone_command", 1000, Callback);
-    rc = pthread_create(&thread, NULL, createSocket,  (void *)1);
+    rc = pthread_create(&thread, NULL, createSocket, 	(void *)1);
     if (rc)
     {
      cout << "Error:unable to create communication thread," << rc << endl;
@@ -121,14 +123,14 @@ int main(int argc, char **argv)
     if(isSocketCreate)
     {
       //cout << "main() : creating write thread, " << i << endl;
-      rc = pthread_create(&writeThread, NULL, writeFunction,  (void *)2);
+      rc = pthread_create(&writeThread, NULL, writeFunction, 	(void *)2);
       if (rc)
       {
         cout << "Error:unable to create write thread," << rc << endl;
         exit(-1);
       }
       // cout << "main() : creating read thread, " << i << endl;
-      rc = pthread_create(&readThread, NULL, readFunction,  (void *)3);
+      rc = pthread_create(&readThread, NULL, readFunction, 	(void *)3);
       if (rc)
       {
         cout << "Error:unable to read create thread," << rc << endl;
@@ -137,7 +139,7 @@ int main(int argc, char **argv)
 
       serviceClient = n.serviceClient<plutodrone::PlutoPilot>("PlutoService",true);
       //cout << "main() : creating service thread, " << i << endl;
-      rc = pthread_create(&serviceThread, NULL, serviceFunction,  (void *)4);
+      rc = pthread_create(&serviceThread, NULL, serviceFunction, 	(void *)4);
       
       if (rc)
       {
