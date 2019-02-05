072a8b707565135bfbd192b23f2020eaab831a40
adiagr <addyagrawal@gmail.com>
2019-02-05 15:59:35 +0530

    Updated file to remove unneccesary publishers

--- plutoserver/scripts/data_via_rosservice.py
+++ plutoserver/scripts/data_via_rosservice.py
@@ -9,13 +9,7 @@ class request_data():
 	def __init__(self):
 		rospy.init_node('drone_board_data')
 		data = rospy.Service('PlutoService', PlutoPilot, self.access_data)
-		#rospy.spin()
-		self.yaw_pub = rospy.Publisher('data/yaw', Int32, queue_size=1)
-		self.roll_pub = rospy.Publisher('data/roll', Int32, queue_size=1)
-		self.pitch_pub = rospy.Publisher('data/pitch', Int32, queue_size=1)
-		self.battery_pub = rospy.Publisher('data/battery', Float64, queue_size=1)
-		self.rssi_pub = rospy.Publisher('data/rssi', Int32, queue_size=1)
-
+		
 	def access_data(self, req):
 		 print "accx = " + str(req.accX), "accy = " + str(req.accY), "accz = " + str(req.accZ)
 		 print "gyrox = " + str(req.gyroX), "gyroy = " + str(req.gyroY), "gyroz = " + str(req.gyroZ)
@@ -24,12 +18,6 @@ class request_data():
 		 print "altitude = " +str(req.alt)
 		 print "battery = " + str(req.battery), "Power Consumed = " + str(req.rssi)
 		 rospy.sleep(.1)
-		 #Uncomment the stuff you want to be explicitly Published
-		 #self.yaw_pub.publish(req.yaw)
-		 #self.roll_pub.publish(req.roll)
-		 #self.pitch_pub.publish(req.pitch)
-		 #self.battery_pub.publish(req.battery)
-		 #self.rssi_pub.publish(req.rssi)
 		 return PlutoPilotResponse(rcAUX2 =1500)
 
 test = request_data()
